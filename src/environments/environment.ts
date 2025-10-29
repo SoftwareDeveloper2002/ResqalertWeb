4213e86c01e49e771edd3ab8f2e9df8083f3121d
Robby Roda <robbyroda.00@gmail.com>
2025-10-30 00:08:28 +0800

    aaa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backendUrl: 'http://127.0.0.1:7000',
+  backendUrl: 'https://resqalertwebbackend-1.onrender.com',
   // backendUrl: 'http://127.0.0.1:7000',
   // backendUrl https://resqalertwebbackend-1.onrender.com
 
